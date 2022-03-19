93b44dd1fb002f198ec8c34c16b095680e18e5b2
Eligiani-Monteiro <eligianedasilva32@gmail.com>
2022-03-19 15:33:49 -0400

    Initial Project

---
+++ src/main/java/com/example/demo/SistemaGerenciamentoDebitosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoDebitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoDebitosApplication.class, args);
+	}
+
+}
